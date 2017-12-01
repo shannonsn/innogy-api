c09923aca8eae9b62bd46bfc651fee02770b6159
Shannon Nichols <shannonsn19@gmail.com>
2017-12-01 16:35:36 +0200

    cross origin setup

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(logger('dev'));
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', '"Origin, X-Requested-With, Content-Type, Accept"');
+  res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials");
   res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
